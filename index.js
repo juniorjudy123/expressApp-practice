480ce8983aab565e1a1949b45c6a873f6c2f7a1a
benoy_Judy <benoyjudy@gmail.com>
2023-12-04 14:03:20 +0530

    minor update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 
-
+//http://localhost:3000/?user=benoy    typed  inthe browser
 const clientRequest = (req, res) => {
     const userInfo = req.query.user;
     console.log(userInfo);
